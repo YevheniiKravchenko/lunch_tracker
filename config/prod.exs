5b6bd59adc4c93299c6ba7bcc6d548fd563c533a
Yevhenii Kravchenko <yevhenii.kravchenko@gmail.com>
2017-09-14 20:01:40 +0300

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :lunch_tracker, LunchTrackerWeb.Endpoint,
 
 
 # Config database
-config :hello, Hello.Repo,
+config :lunch_tracker, LunchTracker.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
